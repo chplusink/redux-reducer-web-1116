792e2e8e5c449ead327a9bea87fde640e1e8eab3
Vic Lam <lam.vic.k@gmail.com>
2017-02-08 16:34:15 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  if ( action.type === 'ADD_FRIEND' ) {
+    let newFriends = [ ...state.friends, action.payload ]
+    return { ...state, friends: newFriends }
+  } else if ( action.type === 'REMOVE_FRIEND' ) {
+    let newFriends = state.friends.filter( function(friend) {
+      return friend.id !== action.payload
+    })
+    return { ...state, friends: newFriends }
+  } else {
+    return state
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
+  if ( action.type === 'INCREASE') {
+    let presents = state.numberOfPresents + 1
+    return { numberOfPresents: presents }
+  } else {
+    return state
+  }
 }
